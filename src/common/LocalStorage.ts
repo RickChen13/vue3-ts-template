22302216cfe19a8be0cb88448bacbd2e618515e5
RickChen13 <rickchen13@gmail.com>
2023-06-26 13:28:06 +0800

    Update LocalStorage.ts

--- src/common/LocalStorage.ts
+++ src/common/LocalStorage.ts
@@ -1,33 +1,28 @@
 class LocalStorage {
-    /**
-     * 设置LocalStorage
-     * 
-     * @param cname 
-     * @param cvalue  
-     */
-    static setLocalStorage(cname: string, cvalue: string) {
-        let result = true
-        try {
-            localStorage.setItem(cname, cvalue)
-        } catch (error) {
-            result = false
-        }
-        return result
+  /**
+   * 设置LocalStorage
+   *
+   * @param cname
+   * @param cvalue
+   */
+  static setLocalStorage(cname: string, cvalue: string): boolean {
+    let result = true;
+    try {
+      localStorage.setItem(cname, cvalue);
+    } catch (error) {
+      result = false;
     }
+    return result;
+  }
 
-    /**
-     * 获取LocalStorage
-     * 
-     * @param cname 
-     */
-    static getLocalStorage(cname: string) {
-        let result = ""
-        const cvalue = localStorage.getItem(cname)
-        if (cvalue != null) {
-            result = cvalue
-        }
-        return result
-    }
+  /**
+   * 获取LocalStorage
+   *
+   * @param cname
+   */
+  static getLocalStorage(cname: string): string | null {
+    return localStorage.getItem(cname);
+  }
 }
 
-export default LocalStorage
+export default LocalStorage;
