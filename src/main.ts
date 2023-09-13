79ace2da0f095fd1a4a990cb8dd506b866adbfea
RickChen13 <rickchen13@gmail.com>
2023-09-13 14:28:48 +0800

    移除vuex

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { createApp } from "vue";
 import App from "./app/App.vue";
-import router from "./app/router";
-
 const app = createApp(App);
 
+import router from "./app/router";
 app.use(router);
-
-// import StarportPlugin from "vue-starport";
-// app.use(StarportPlugin({ keepAlive: true }));
-
-// import store from "./app/store";
-// app.use(store);
 
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
