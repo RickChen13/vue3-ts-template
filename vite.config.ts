e4f8e6bcc4c90c8b8dd7298ef8fe045dbfb54778
RickChen13 <rickchen13@gmail.com>
2024-11-18 11:33:06 +0800

    mod:同步更改

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+        },
+    }, server: {
+        host: "127.0.0.1"
     }
 });
